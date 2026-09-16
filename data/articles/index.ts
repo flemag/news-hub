3c10efc6a675d4220ab8e03ac3ba862c6e169a1b
flemag <46199867+flemag@users.noreply.github.com>
2026-09-16 08:06:55 +0200

    articles: lot 2026-09-g (10 papiers du 15-16 sept.)

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -1,6 +1,6 @@
 /**
  * Index articles. Automation : TOUJOURS écrire dans un fichier du jour
- * (ex. 2026-09-f.json) et l’ajouter ici. Ne JAMAIS remplacer un fichier
+ * (ex. 2026-09-g.json) et l’ajouter ici. Ne JAMAIS remplacer un fichier
  * existant par un seul article.
  */
 import month202609 from "./2026-09.json";
@@ -9,10 +9,12 @@ import month202609c from "./2026-09-c.json";
 import month202609d from "./2026-09-d.json";
 import month202609e from "./2026-09-e.json";
 import month202609f from "./2026-09-f.json";
+import month202609g from "./2026-09-g.json";
 
 import type { Article } from "@/lib/types";
 
 const MONTH_FILES: Article[][] = [
+  month202609g as Article[],
   month202609f as Article[],
   month202609d as Article[],
   month202609c as Article[],
