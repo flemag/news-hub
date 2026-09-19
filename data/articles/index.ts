da12c40ce08caa7bab6e8ef4eff251a872663c20
flemag <46199867+flemag@users.noreply.github.com>
2026-09-19 08:28:28 +0200

    articles: lot 2026-09-j (19 sept.)

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -12,10 +12,12 @@ import month202609f from "./2026-09-f.json";
 import month202609g from "./2026-09-g.json";
 import month202609h from "./2026-09-h.json";
 import month202609i from "./2026-09-i.json";
+import month202609j from "./2026-09-j.json";
 
 import type { Article } from "@/lib/types";
 
 const MONTH_FILES: Article[][] = [
+  month202609j as Article[],
   month202609i as Article[],
   month202609h as Article[],
   month202609g as Article[],
